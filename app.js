1c821c5be0f92aedf3ae796b187072bd7a611a5d
tuyipiao <tzx@syzc.net.cn>
2018-10-09 19:04:13 +0800

    这是一次测试

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-	//测试
+	//测试lala
     // 登录
     wx.login({
       success: res => {
