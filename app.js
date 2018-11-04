fb17995481cfa6684bf9564b0cf031b59b1a7553
wangyu <1148685323@qq.com>
2018-11-04 10:58:31 +0800

    页面大小

--- app.js
+++ app.js
@@ -8,12 +8,25 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    //测试lala
-    // 登录
+    //根据code获取openid等信息
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-
+      //获取code
+      success: function (res) {
+        var code = res.code; //返回code
+        console.log(code);
+        var appId = 'wx3f4ccb9e9f33c7cf';
+        var secret = '...';
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appId + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+          data: {},
+          header: {
+            'content-type': 'json'
+          },
+          success: function (res) {
+            var openid = res.data.openid //返回openid
+            console.log('openid为' + openid);
+          }
+        })
       }
     })
     // 获取用户信息
