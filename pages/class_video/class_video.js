edb4e5033c5aa6f422f8f374fcfe8d53a8595fbd
““郑修月” <1586815627@qq.com>
2019-04-04 19:39:46 +0800

    zxy0404

---
+++ pages/class_video/class_video.js
@@ -0,0 +1,40 @@
+// pages/class_video/class_video.js
+var app = getApp();
+var requestIP = app.globalData.requestIP
+Component({
+  /**
+   * 组件的属性列表
+   */
+
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    //宣传视频
+    discovery: {
+      video: 'https://v.mifile.cn/b2c-mimall-media/ed921294fb62caf889d40502f5b38147.mp4',
+      image: "https://i1.mifile.cn/a4/xmad_15278351912522_frJQc.jpg",//封面图
+      duration_raw: 74//秒数
+    },
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+
+  ready: function () {
+
+  }
+})
+
+
+
+
