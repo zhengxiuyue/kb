ca8ce0b88c34efe9f41af746aed8de0695b005be
wangyu <1148685323@qq.com>
2019-06-01 12:02:41 +0800

    修改

--- pages/store_order/store_order.js
+++ pages/store_order/store_order.js
@@ -31,10 +31,6 @@ Page({
     username: '',
     tel: '',
     primarytel: '',
-    authcode: '',
-    time1: '获取验证码', //倒计时 
-    currentTime: 60,//限制60s
-    isClick: false,//获取验证码按钮，默认允许点击
     visible5: false,
     actions5: [
       {
@@ -130,69 +126,7 @@ Page({
   //获取手机号码
   telInput: function (e) {
     var that = this;
-    if (e.detail.value == that.data.primarytel) {
-      that.setData({
-        auth: "none"
-      });
-    }
-    else {
-      that.setData({
-        auth: "block"
-      });
-    }
     this.setData({ tel: e.detail.value })
-  },
-
-  //获取验证码
-  authcodeInput: function (event) {
-    this.setData({ authcode: event.detail.value })
-  },
-
-  //验证
-  gainAuthCodeAction: function () {
-    let that = this;
-    //第一步：验证手机号码
-    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;// 判断手机号码的正则
-    if (that.data.tel.length == 0) {
-      wx.showToast({
-        title: '请填写正确的手机号码!',
-        icon: 'none',
-        duration: 2000
-      })
-      return false;
-    }
-
-    else if (!myreg.test(that.data.tel)) {
-      wx.showToast({
-        title: '请填写正确的手机号码!',
-        icon: 'none',
-        duration: 2000
-      })
-      return false;
-    }
-    that.sendCode();
-
-    //第二步：设置计时器
-    // 先禁止获取验证码按钮的点击
-    that.setData({
-      isClick: true,
-    })
-    //60s倒计时 setInterval功能用于循环，常常用于播放动画，或者时间显示
-    var currentTime = that.data.currentTime;
-    interval = setInterval(function () {
-      currentTime--;//减
-      that.setData({
-        time1: currentTime + '秒后获取'
-      })
-      if (currentTime <= 0) {
-        clearInterval(interval)
-        that.setData({
-          time1: '重新发送',
-          currentTime: 60,
-          isClick: false
-        })
-      }
-    }, 1000);
   },
 
   //预约
@@ -241,66 +175,6 @@ Page({
         that.order("*noneedforcode*");
         }
     }
-
-    else if (that.data.auth == "block") {
-      if (that.data.authcode.length == 0) {
-        wx.showToast({
-          title: '请填写验证码！',
-          icon: 'none',
-          duration: 2000
-        })
-        return false;
-      }
-      else {
-        if (that.data.date == "点击选择预约时间") {
-          wx.showToast({
-            title: '请选择预约时间!',
-            icon: 'none',
-            duration: 2000
-          })
-          return false;
-        }
-        else {
-        that.order(that.data.authcode);
-        }
-      }
-    }
-  },
-  //发送验证码
-  sendCode: function (e) {
-    var that = this;
-
-    wx.request({
-      url: requestIP + '/student/sendCode',
-      data: {
-        phone: that.data.tel,
-        type: 2
-      },
-      method: 'POST',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'userid': app.globalData.userid
-      },// 设置请求的 header
-      success: function (res) {
-        if (res.data.resultCode == "101") {
-        } else {
-          wx.showToast({
-            title: '验证码发送失败!',
-            icon: 'none',
-            duration: 2000
-          })
-        }
-      },
-      fail: function (res) {
-        wx.showToast({
-          title: '验证码发送失败!',
-          icon: 'none',
-          duration: 2000
-        })
-      }
-    })
-  },
-  onPullDownRefresh: function () {
   },
 
   //预约
@@ -327,15 +201,6 @@ Page({
       },
       success(res) {
         if (res.data.resultCode == '101') {
-          /*$Message({
-            content: '预约成功！',
-          });
-          setTimeout(function () {
-            //跳到更多页面
-            wx.redirectTo({
-              url: '/pages/more/more',
-            })
-          }, 2000)*/
           wx.navigateTo({
             url: '/pages/message/message?flag=1',
           })
